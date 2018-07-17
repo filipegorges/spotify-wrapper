763bef367af3f01aa88dc0d5c7617690171b9c15
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-07-17 17:10:41 -0300

    Update examples

--- examples/album.js
+++ examples/album.js
@@ -1,7 +1,13 @@
+/* to run: babel-node albums.js */
+
 global.fetch = require('node-fetch');
 
-import { searchAlbums } from '../src/main';
+import SpotifyWrapper from '../src/index';
 
-const albums = searchAlbums('Incubus');
+const spotify = new SpotifyWrapper({
+  token: 'BQBM8B0Cn-wVVZjXcXXA0lMGheT4KsxTqFJ8HnCB6Vzqk4AJs1XMndgOv2zDhuoL02sbu6jMj2WVuLAuFO9HEodtCErGyxi5iXBEGydQC8cvbIFw4ohXQwGbmm5LL8JEYeNH8JMrq4Cqy3PS'
+});
 
-albums.then(data => console.log(data));
+const albums = spotify.search.albums('Incubus');
+
+albums.then(data => data.albums.items.map(item => console.log(item.name)));
